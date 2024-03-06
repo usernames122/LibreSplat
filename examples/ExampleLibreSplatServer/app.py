53b6bedb881b5d84a54391852faca537a7136e62
Usernames122 <sigge.arkestal@gmail.com>
2024-03-06 15:42:10 +0100

    Made example server cleaner

--- examples/ExampleLibreSplatServer/app.py
+++ examples/ExampleLibreSplatServer/app.py
@@ -1,19 +1,22 @@
-from flask import Flask,request
+from flask import Flask, request
 from base64 import b64decode
 from random import randint
 import os
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-@app.route("/upload",methods=["POST"])
+
+
+@app.route("/upload", methods=["POST"])
 def handle_response():
     json_data = request.get_json(silent=False)
-    _id = str(randint(1,99999)) + str(randint(1,99999))
+    _id = str(randint(1, 99999)) + str(randint(1, 99999))
     os.mkdir("uploaded_files/" + _id)
-    with open("uploaded_files/" + _id + "/snapshot.pickle","wb"):
+    with open("uploaded_files/" + _id + "/snapshot.pickle", "wb"):
         f.write(b64decode(json_data["snapshot"].encode()))
-    with open("uploaded_files/" + _id + "/log.txt","wb"):
+    with open("uploaded_files/" + _id + "/log.txt", "wb"):
         f.write(b64decode(json_data["log"].encode()))
     return "<p>success</p>"
