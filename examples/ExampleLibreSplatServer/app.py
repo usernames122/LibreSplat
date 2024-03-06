b68650355a9d59aa5f9b87ef61f7d0999a1b87b1
Usernames122 <sigge.arkestal@gmail.com>
2024-03-06 15:48:18 +0100

    fixed some stuff

--- examples/ExampleLibreSplatServer/app.py
+++ examples/ExampleLibreSplatServer/app.py
@@ -15,8 +15,8 @@ def handle_response():
     json_data = request.get_json(silent=False)
     _id = str(randint(1, 99999)) + str(randint(1, 99999))
     os.mkdir("uploaded_files/" + _id)
-    with open("uploaded_files/" + _id + "/snapshot.pickle", "wb"):
+    with open("uploaded_files/" + _id + "/snapshot.pickle", "wb")as f:
         f.write(b64decode(json_data["snapshot"].encode()))
-    with open("uploaded_files/" + _id + "/log.txt", "wb"):
+    with open("uploaded_files/" + _id + "/log.txt", "wb") as f:
         f.write(b64decode(json_data["log"].encode()))
     return "<p>success</p>"
